70062a478b0d96ecfdcd68c46f7c0bb392a4f3d2
MateuszSliwinski <167821763+Sliww@users.noreply.github.com>
2024-06-14 17:00:05 +0200

    CODICE COMPLETO
    Implemento della variabile SCORE

--- script.js
+++ script.js
@@ -98,34 +98,16 @@ const questions = [
   },
 ];
 
-// TIMER CREATO CON MARCO
-
-/*const interval = setInterval(()=>{
-  countDown.innerText = questionInterval;
-  questionInterval--;
-
-  if (questionInterval < 0){
-    //clearInterval(interval);
-    questionInterval = INITIAL_COUNT_DOWN_TIME;
-    questionNumber++;
-      printAnswers(questionNumber);  
-  }
-}, 1000);*/
-
-
-// INTANTO APPENA IL CODICE VERRA' FINITO DOBBIAMO DARE UNA PICCOLA ORDINATA AL CODICE
-// METTIAMO TUTTE LE FUNZIONI VICINE, LO ORDINIAMO IN MODO LOGICO E METTIAMO TUTTI I RICHIAMI
-// DEL DOM NELLA PARTE SUPERIORE COME ANDREBBE FATTO
-
-
-// TIMER
-// DIVIDO IL TIMER CREATO CON MARCO IN 2 FUNZIONI IN MODO TALE CHE ABBIA IL RESET DEL TIMER SEPARATO 
-// E DENTRO AL SET TIMER DO LE CONDIZIONI CHE SE DOVESSE ARRIVARE ALLA FINE DELLE DOMANDE SI STOPPA E PARTE LA FUNZIONE DI STAMPA 
-
-
 let questionInterval = 60;
 const INITIAL_COUNT_DOWN_TIME = questionInterval;
 const interval = setInterval(setTimer, 1000);
+let labels = document.querySelectorAll(".answerLabel");
+let inputButtons = document.querySelectorAll(".inputButtons");
+let printedQuestion = document.createElement("span");
+let questionNumberSpan = document.querySelector("#questionNumber");
+let questionNumber = 0;
+let answersArray = [];
+let score = 0;
 
 // RESETTA IL TIMER A 60 SECS
 
@@ -150,17 +132,12 @@ function setTimer(){
   if (questionNumber >= 10){
     stopExam();
   }
-
 }
 
 
 // ora creo funzione che fa fermare il test e stampa tutti i risultati!
 
-// STOPPA L'ESAME COMINCIANDO A FARE UN CLEAR INTERVAL POI FA SCOMPARIRE TUTTE I DIV ECC...
-// E FA COMPARIRE IL DIV PER I RISULTATI CON ALLA FINE LA FUNZIONE SHOWRESULTS
-
 function stopExam(){
-  
   if (questionNumber >= 10){
     clearInterval(interval);
     document.querySelector(".inputContainer").classList.add("hiddenInput");
@@ -171,19 +148,9 @@ function stopExam(){
     printedQuestion.textContent = "";
     document.querySelector("#scoreResults").classList.remove("hiddenInput");
     document.querySelector("#scoreResultsSpan").classList.remove("hiddenInput");
-    // ora mi creo una mini funzione per la stampa perchè qua ce troppa roba e mi sparo la funzione come ultima cosa
     showResults();
   }
 }
-
-// POTEVO FARLA DIRETTAMENTE NELL'IF MA MI SEMBRAVA PIù ORDINATO FARLA A PARTE ! 
-// FUNZIONE CON LE CONDIZIONI DEI VOTI DA 0 A 5 E DA 6 A 10 
-// LE CONDIZIONI DELLA STAMPA VOTI CONTENGONO LA VARIABILE SCORE CHE ANCORA DOBBIAMO RIUSCIRE A INCREMENTARE 
-// QUANDO VIENE SELEZIONATA LA RISPOSTA CORRETTA, MA INTANTO E' TUTTO PRONTO. 
-// SE IN MATTINATA RIESCI A FARE QUEST'ULTIMA COSA ABBIAMO FINITO CREDO.
-
-let scoreResultsSpan = document.querySelector("#scoreResultsSpan");
-let scoreResults = document.querySelector("#scoreResults");
 
 function showResults(){
   if (score < 6){
@@ -193,16 +160,7 @@ function showResults(){
   }
 }
 
-let labels = document.querySelectorAll(".answerLabel");
-let inputButtons = document.querySelectorAll(".inputButtons");
-let printedQuestion = document.createElement("span");
-let questionNumberSpan = document.querySelector("#questionNumber");
-let questionNumber = 0;
-let answersArray = [];
-let score = 7;
 
-
-//
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -213,7 +171,6 @@ function shuffleArray(array) {
 }
 
 function printAnswers(questionIndex) {
-
   let currentQuestion = questions[questionIndex];
   answersArray = shuffleArray([
     currentQuestion.correct_answer,
@@ -229,19 +186,15 @@ function printAnswers(questionIndex) {
   });
 
   if (answersArray.length <= 2) {
-    // Disabilita e nasconde check3 e check4
-    console.log(answersArray.length);
     document.getElementById("check3").setAttribute("disabled", "disabled");
     document.getElementById("check4").setAttribute("disabled", "disabled");
     document.getElementById("button3").classList.add("hiddenInput");
     document.getElementById("button4").classList.add("hiddenInput");
   } else {
-    // Assicura che check3 e check4 siano abilitati e visibili
     document.getElementById("check3").removeAttribute("disabled");
     document.getElementById("check4").removeAttribute("disabled");
     document.getElementById("button3").classList.remove("hiddenInput");
     document.getElementById("button4").classList.remove("hiddenInput");
-
   }
 
   printedQuestion.textContent = currentQuestion.question;
@@ -254,23 +207,19 @@ function printAnswers(questionIndex) {
 printAnswers(questionNumber);
 
 inputButtons.forEach((inputButton, index) => {
-  inputButton.addEventListener("change", function () {
+  inputButton.addEventListener("click", function () {
     if (inputButton.checked) {
-
-//ASSEGNO IL RESET DEL TIMER AL CLICK DELLE RADIO
-
       resetTimer();
+      if (labels[index].textContent === questions[questionNumber].correct_answer) {
+        score++;
+      }
       questionNumber++;
       inputButtons.forEach(input => input.checked = false);
       if (questionNumber < questions.length) {
         printAnswers(questionNumber);
+      } else {
+        stopExam();
       }
     }
   });
 });
-
-// LO SCRIVO QUA COSA HO FATTO NELL'HTML E CSS
-// HO DATO LE CLASSI HIDDEN INPUT AL DIV E SPAN DI SHOW RESULTS E QUANDO L'ESAME FINISCE LA CLASSE VIENE
-// ELIMINATA INVECE PER GLI ALTRI DIV VIENE AGGIUNTA 
-// NEL CSS HO DOVUTO DARE A HIDDENINPUT LA DICITURA "!IMPORTANT" IN QUANTO IL TIMER AVEVA QUALCHE REGOLA
-// CSS CHE MI SOVRASCRIVEVA IL DISPLAY NONE E NON RIUSCIVO A TROVARLA, CON IMPORTANT SONO RIUSCITO A FARLO SPARIRE.
